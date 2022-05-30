e9a3357350869f804a4dec41ac18859d9e381b34
adi1111 <holubeckiadrian@gmail.com>
2022-05-30 14:53:05 +0200

    Basic layout and script. Rainbow and shadow functionality will be updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # etch-a-sketch
+
+
+Project of etch-a-sketch application based on JavaScript.
+
+Porject as a part of Web Developement course "The Odin Project"
---
+++ script.js
@@ -0,0 +1,85 @@
+let initialValue = 100;
+const radioValues = document.querySelectorAll('input[name=size]');
+let pixelSize = initialValue;
+let pixel = document.querySelectorAll('.pixel');
+let color = document.querySelector('#color');
+
+
+// event listener for chosing size value and setting new canvas
+for (let i = 0; i < 4; i++) {
+radioValues[i].addEventListener('click', function(e){
+    initialValue = Number((e.target.value));
+    pixelSize = initialValue;
+
+    if (document.querySelectorAll('.pixel').length > 0) {
+        const existingPixel = document.querySelectorAll('.pixel')
+
+        for(let i = 0; i < existingPixel.length;i++){
+        existingPixel[i].remove();
+        }
+    }
+
+    for (let i = 0; i < pixelSize; i++){
+        let container = document.querySelector('.container');
+        let pixel = document.createElement('div');
+        pixel.setAttribute('class', 'pixel');
+        
+        if (pixelSize == 400) {
+            pixel.style.height = "25px";
+            pixel.style.width = "25px";
+        }
+        
+        if (pixelSize == 1600) {
+            pixel.style.height = "12.5px";
+            pixel.style.width = "12.5px";
+        }
+        
+        if (pixelSize == 4096) {
+            pixel.style.height = "7.8125px";
+            pixel.style.width = "7.8125px";
+        }
+        container.appendChild(pixel);
+        }
+        pixel = document.querySelectorAll('.pixel');
+})
+    
+}
+
+radioValues[0].click();
+
+// painting script 
+
+const touchpad = document.querySelector('.container');
+
+function showup(pix){
+    if (pix.type == 'mouseout'){
+        pix.target.style.backgroundColor = `${color.value}`;
+    }
+ }
+
+touchpad.addEventListener('mousedown',function(){
+    console.log('down');
+    for(let i = 0; i < pixel.length;i++){
+        pixel[i].addEventListener('mouseout', showup,true)
+    }
+    ;
+}, true);
+
+touchpad.addEventListener('mouseup',function(){
+    for(let i = 0; i < pixel.length;i++){
+    pixel[i].removeEventListener('mouseout',showup,true)
+};
+}, true);
+
+// Clearing canvas 
+
+const clear = document.querySelector('#clear');
+
+function clearing() {
+    for(let i = 0; i < pixel.length;i++) {
+        pixel[i].style.backgroundColor = "white";
+    }
+}
+
+clear.addEventListener('click', clearing);
+
